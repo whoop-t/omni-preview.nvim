5c3717cae84dd1d8f71ca6d59a6043f4fc853b94
SylvanFrankin <sylvanfranklin@icloud.com>
2025-01-18 15:25:27 -0500

    refactor: remove opts feild

--- lua/omni-preview/extras.lua
+++ lua/omni-preview/extras.lua
@@ -1,39 +1,30 @@
 local M = {}
 
 M.previews = {
-    { name = "typst-preview", trig = "typst", start = "TypstPreview" },
+    { name = "typst-preview", trig = "typst", start = "TypstPreview", stop = "TypstPreviewStop" },
     { name = "vimtex",        trig = "tex",   start = "LatexStart" },
     {
         name = "csvview",
         trig = "csv",
         start = "CsvViewEnable",
         stop = "CsvViewDisable",
-        opts = {
-            view = {
-                spacing = 2,
-                display_mode = "border",
-            },
-        }
     },
     {
         name = "peek",
         trig = "markdown",
         start = function() require "peek".open() end,
         stop = function() require "peek".close() end,
-        opts = { app = "browser" }
     },
 }
 
 function M.setup()
-    -- Function to safely require and setup a module
     local function safe_setup(prev)
         local status, module = pcall(require, prev.name)
         if status and module and module.setup then
-            module.setup(prev.opts)
+            module.setup(prev)
         end
     end
 
-    -- Configure each module
     for _, p in ipairs(M.previews) do
         safe_setup(p)
     end
