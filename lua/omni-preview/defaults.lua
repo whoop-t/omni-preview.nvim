994393014f190272d8376d8c372717bf96ad0dc7
SylvanFranklin <sylvanfranklin@icloud.com>
2025-01-22 11:14:37 -0500

    refactor: merge extras and builtin

--- lua/omni-preview/defaults.lua
+++ lua/omni-preview/defaults.lua
@@ -1,4 +1,4 @@
-local npm = require "omni-preview.npm"
+-- local npm = require "omni-preview.npm"
 local M = {}
 
 M.system_open = function()
@@ -10,12 +10,25 @@ M.system_open = function()
 end
 
 M.previews = {
+    { name = "typst-preview", trig = "typst", start = "TypstPreview", stop = "TypstPreviewStop" },
+    { name = "vimtex",        trig = "tex",   start = "LatexStart" },
+    {
+        name = "csvview",
+        trig = "csv",
+        start = "CsvViewEnable",
+        stop = "CsvViewDisable",
+    },
+    {
+        name = "peek",
+        trig = "markdown",
+        start = function() require "peek".open() end,
+        stop = function() require "peek".close() end,
+    },
     { trig = "pdf",  start = M.system_open, name = "builtin" },
     { trig = "svg",  start = M.system_open, name = "builtin" },
     { trig = "png",  start = M.system_open, name = "billtin" },
     { trig = "jpeg", start = M.system_open, name = "builtin" },
     { trig = "html", start = M.system_open, name = "builtin" },
-    npm,
 }
 
 return M
--- lua/omni-preview/init.lua
+++ lua/omni-preview/init.lua
@@ -1,11 +1,9 @@
 local M = {}
 function M.setup(opts)
     opts = opts or {}
-    local defaults = require("omni-preview.defaults")
+    local defaults = require("omni-preview.defaults").previews
     local commands = require("omni-preview.commands")
-    local extras = require("omni-preview.extras")
-    M.previews = vim.tbl_deep_extend("force", defaults.previews, opts.previews or {})
-    M.previews = vim.tbl_deep_extend("force", M.previews, extras.previews or {})
+    M.previews = vim.tbl_deep_extend("force", defaults, opts.previews or {})
     vim.api.nvim_create_user_command("OmniPreviewStart", commands.start, { desc = "Start preview for current filetype" })
     vim.api.nvim_create_user_command("OmniPreviewStop", commands.stop, { desc = "Stop preview for current filetype" })
     vim.api.nvim_create_user_command("OmniPreviewToggle", commands.toggle,
